c9eadc6c876000aebe2b9c0e30eaa196b2dd6d80
Furkan Ergin <s325881@oslomet.no>
2019-06-20 16:00:24 +0200

    Start

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using WebGrease.Css.Extensions;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+    }
+}
